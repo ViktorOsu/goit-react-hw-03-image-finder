481a2ca82986adceef1d8ff1e5debea684cfb0eb
ViktorOsu <123021878+ViktorOsu@users.noreply.github.com>
2023-05-04 13:34:59 +0300

    Load More

--- src/components/ImageGallery/ImageGallery.styled.jsx
+++ src/components/ImageGallery/ImageGallery.styled.jsx
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 export const ImgGallery = styled.ul`
   display: grid;
   max-width: calc(100vw - 48px);
-  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(319px, 1fr));
   grid-gap: 16px;
   margin-top: 0;
   margin-bottom: 0;
